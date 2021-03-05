131b35256877511058a1a7cfafc1190d27698120
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-03-05 13:39:44 +0100

    exo fini

---
+++ Classes/DB.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Handle MySQL Connection with PDO.
+ * Class DB
+ */
+class DB
+{
+    private string $server = 'localhost';
+    private string $db = 'blog';
+    private string $user = 'root';
+    private string $pwd = '';
+
+    private static ?PDO $dbInstance = NULL;
+
+    /**
+     * DbStatic constructor.
+     */
+    public function __construct() {
+        try {
+            self::$dbInstance = new PDO("mysql:host=$this->server;dbname=$this->db;charset=utf8", $this->user, $this->pwd);
+            self::$dbInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$dbInstance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $exception) {
+            echo $exception->getMessage();
+        }
+    }
+
+    /**
+     * Return PDO instance.
+     */
+    public static function getInstance(): ?PDO {
+        if( is_null(self::$dbInstance) ) {
+            new self();
+        }
+        return self::$dbInstance;
+    }
+
+    /**
+     * Avoid instance to be cloned.
+     */
+    public function __clone() {}
+}
--- index.php
+++ index.php
@@ -15,17 +15,44 @@
  *
  * A l'aide d'une boucle, affichez chaque ligne du tableau de résultat.
  */
+ini_set('display_errors', '1');
 
 // TODO Votre code ici.
+require "./Classes/DB.php";
+$conn = DB::getInstance();
 
-
+$join1 = $conn->prepare("
+        SELECT article.id, article.title, article.content, categorie.name 
+        FROM article
+            INNER JOIN categorie ON category_fk = categorie.id
+        ");
+$join1->execute();
+foreach ($join1->fetchAll() as $item){
+    echo "id: ".$item["id"]."<br>";
+    echo "titre: ".$item["title"]."<br>";
+    echo "contenu: ".$item["content"]."<br>";
+    echo "nom de la categorie: ".$item["name"]."<br>";
+}
+echo "<br>";
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
  */
 
 // TODO Votre code ici.
 
-
+$joinAlias = $conn->prepare("
+        SELECT ar.id as i, ar.title as t, ar.content as c, c.name as n 
+        FROM article as ar
+            INNER JOIN categorie as c ON ar.category_fk = c.id
+        ");
+$joinAlias->execute();
+foreach ($joinAlias->fetchAll() as $item){
+    echo "id: ".$item["i"]."<br>";
+    echo "titre: ".$item["t"]."<br>";
+    echo "contenu: ".$item["c"]."<br>";
+    echo "nom de la categorie: ".$item["n"]."<br>";
+}
+echo "<br>";
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
  *    Ajoutez des commentaires et liez un utilisateur au commentaire.
@@ -33,3 +60,42 @@
  */
 
 // TODO Votre code ici.
+
+$first = "Bidule";
+$last = "Truc";
+$mail = "mail.mail@mail.mail";
+$psw = "securite1234";
+
+$insertUser = $conn->prepare("INSERT INTO utilisateur (firstName, lastName, mail, password) 
+                                VALUES (:first, :last, :mail, :psw)"
+                        );
+$insertUser->bindParam(":first", $first);
+$insertUser->bindParam(":last", $last);
+$insertUser->bindParam(":mail", $mail);
+$insertUser->bindParam(":psw", $psw);
+$insertUser->execute();
+
+$content = "comentaire constructif toussa";
+$user = 1;
+$article = 2;
+
+$insertCom = $conn->prepare("INSERT INTO commentaire (content, user_fk, article_fk) 
+                                   VALUES (:content, :user, :article)"
+                            );
+$insertCom->bindParam(":content", $content);
+$insertCom->bindParam(":user", $user);
+$insertCom->bindParam(":article", $article);
+$insertCom->execute();
+
+$leftJoin = $conn->prepare("
+        SELECT c.content, u.firstName, u.lastName
+        FROM commentaire as c
+            LEFT JOIN utilisateur as u ON c.user_fk = u.id  
+        ");
+
+$leftJoin->execute();
+foreach ($leftJoin->fetchAll() as $item){
+    echo "contenu du comentaire: ".$item["content"]."<br>";
+    echo "nom: ".$item["lastName"]."<br>";
+    echo "Prenom: ".$item["firstName"]."<br>";
+}
